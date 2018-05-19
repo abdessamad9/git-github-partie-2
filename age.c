e98e31294dc1b1bb8702262aca829ff18e1ec8ca
abdessamad9 <abdessamadfettar98@gmail.com>
2018-05-19 02:49:43 +0200

    ajout du script de notre programme .c avant de le compiler

---
+++ age.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	printf("entrez votre date de naissance svp : ");
+	int date_de_naissance;
+	scanf("%d",&date_de_naissance);
+	printf("\n vous avez %d ans.", 2018 - date_de_naissance);
+}
+
