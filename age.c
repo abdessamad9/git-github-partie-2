d27ed10676cb38144cdb6dce678d4c3f0fc53cba
abdessamad9 <abdessamadfettar98@gmail.com>
2018-05-19 02:53:37 +0200

    modification de age.c en pour une mise en page plus propre

--- age.c
+++ age.c
@@ -5,7 +5,7 @@ int main(){
 	printf("entrez votre date de naissance svp : ");
 	int date_de_naissance;
 	scanf("%d",&date_de_naissance);
-	printf("\n vous avez %d ans.", 2018 - date_de_naissance);
+	printf("\n vous avez %d ans.\n", 2018 - date_de_naissance);
 	return 0;
 }
 
